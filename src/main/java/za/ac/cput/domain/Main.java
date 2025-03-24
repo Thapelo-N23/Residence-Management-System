e2c19cf71c1a586e4c5f3bee28c1be5520ef7a5e
tsireledzonetshilonwe <165159482+tsireledzonetshilonwe@users.noreply.github.com>
2025-03-24 21:14:35 +0200

    Add files via upload

--- src/main/java/za/ac/cput/domain/Main.java
+++ src/main/java/za/ac/cput/domain/Main.java
@@ -1,7 +1,8 @@
 package za.ac.cput.domain;
 
 import za.ac.cput.domain.factory.ResidenceFactory;
-import za.ac.cput.factory.PaymentFactory;
+import za.ac.cput.domain.factory.StaffFactory;
+//import za.ac.cput.factory.PaymentFactory;
 
 
 public class Main {
@@ -14,15 +15,21 @@ public class Main {
           System.out.println("leader is null");
           }
 
-
-        Payment payment = PaymentFactory.createPayment(
-                "PAY123456", "2500", false, "2005-03-04", "1001");
-
-        if (payment != null) {
-            System.out.println("Payment successfully created: " + payment);
-        } else {
-            System.out.println("Failed to create Payment");
-        }
+        Staff staff = StaffFactory.createStaff("213213","Tsireledzo","Mbedzi","tsireledzombedzi@gmail.com","worker");
+          if(staff != null){
+              System.out.println("staff was created: " + staff);
+          }
+          else{
+              System.out.println("Staff is null");
+          }
+//        Payment payment = PaymentFactory.createPayment(
+//                "PAY123456", "2500", false, "2005-03-04", "1001");
+//
+//        if (payment != null) {
+//            System.out.println("Payment successfully created: " + payment);
+//        } else {
+//            System.out.println("Failed to create Payment");
+//        }
     }
 }
 
