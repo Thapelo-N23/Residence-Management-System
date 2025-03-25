2c83d2f006822c11955215c88578b83e78a65eb2
Olwethu-Nene <165093989+Olwethu-Nene@users.noreply.github.com>
2025-03-25 04:19:20 +0200

    Add files via upload

--- src/main/java/za/ac/cput/domain/Main.java
+++ src/main/java/za/ac/cput/domain/Main.java
@@ -1,11 +1,11 @@
 package za.ac.cput.domain;
 
+import za.ac.cput.domain.Room;
 import za.ac.cput.domain.factory.ResidenceFactory;
+import za.ac.cput.domain.factory.RoomFactory;
 import za.ac.cput.domain.factory.StaffFactory;
 import za.ac.cput.domain.factory.PaymentFactory;
-import za.ac.cput.domain.factory.MaintenanceRequestFactory;
-import za.ac.cput.domain.factory.StudentFactory;
-import java.time.LocalDateTime;
+
 
 public class Main {
 
@@ -33,33 +33,13 @@ public class Main {
             System.out.println("Failed to create Payment");
         }
 
-        LocalDateTime requestDate = LocalDateTime.now();
-        LocalDateTime completionDate = LocalDateTime.now().plusDays(2);
-
-        MaintenanceRequest request = new MaintenanceRequest.Builder("MR101", "STU230640", "R001",
-                "Broken door", LocalDateTime.now())
-                .status("In Progress")
-                .staffId("213213")
-                .resolutionNotes("Resolved by replacing the door handle")
-                .completionDate(LocalDateTime.now().plusHours(2))
-                .build();
+        Room room = RoomFactory.createRoom("R002", 002, "Single", "Occupied", 2, "Eldorado");
+        System.out.println(room);
 
 
-        if (request != null) {
-            System.out.println("Maintenance Request Created Successfully:");
-            System.out.println(request);
-        } else {
-            System.out.println("!Failed to create Maintenance Request. Please check input values.");
-        }
-
-       Student student = StudentFactory.createStudent(
-               "STU230640", "Lisa", "Ngozi", "lisa.ngozi@gmail.com",
-"0812345678", true, "R002", "123456789");
-
-        if (student != null) {
-            System.out.println("Student is successfully created: " + student);
-        } else {
-           System.out.println("Student is null");
         }
     }
-}
+
+
+
+
