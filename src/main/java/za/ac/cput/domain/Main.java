cf5632eaf6f17ca2a4fa255d99b4c8bf87f6d785
Onako Ntsaluba <230741754@mycput.ac.za>
2025-03-25 04:16:02 +0200

    Updated classes

--- src/main/java/za/ac/cput/domain/Main.java
+++ src/main/java/za/ac/cput/domain/Main.java
@@ -33,8 +33,8 @@ public class Main {
             System.out.println("Failed to create Payment");
         }
 
-        LocalDateTime.now();
-        LocalDateTime.now();
+        LocalDateTime requestDate = LocalDateTime.now();
+        LocalDateTime completionDate = LocalDateTime.now().plusDays(2);
 
         MaintenanceRequest request = new MaintenanceRequest.Builder("MR101", "STU230640", "R001",
                 "Broken door", LocalDateTime.now())
