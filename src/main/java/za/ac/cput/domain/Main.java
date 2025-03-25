68c1e6639b9f84cb2b8a3e85eb802e44aa1f4149
Onako Ntsaluba <230741754@mycput.ac.za>
2025-03-25 04:06:43 +0200

    Updated classes

--- src/main/java/za/ac/cput/domain/Main.java
+++ src/main/java/za/ac/cput/domain/Main.java
@@ -1,9 +1,10 @@
 package za.ac.cput.domain;
 
-import za.ac.cput.domain.factory.MaintenanceRequestFactory;
 import za.ac.cput.domain.factory.ResidenceFactory;
 import za.ac.cput.domain.factory.StaffFactory;
 import za.ac.cput.domain.factory.PaymentFactory;
+import za.ac.cput.domain.factory.MaintenanceRequestFactory;
+//import za.ac.cput.domain.factory.StudentFactory;
 import java.time.LocalDateTime;
 
 public class Main {
@@ -43,14 +44,22 @@ public class Main {
                 .completionDate(LocalDateTime.now().plusHours(2))
                 .build();
 
+
         if (request != null) {
             System.out.println("Maintenance Request Created Successfully:");
             System.out.println(request);
         } else {
             System.out.println("!Failed to create Maintenance Request. Please check input values.");
         }
+
+       // Student student = StudentFactory.createStudent(
+              //  "STU230640", "Lisa", "Ngozi", "lisa.ngozi@gmail.com",
+//"0812345678", true, "R002", "123456789");
+
+        //if (student != null) {
+          //  System.out.println("Student is successfully created: " + student);
+        //} else {
+           // System.out.println("Student is null");
+        //}
     }
 }
-
-
-
