8cd06fb6bdfe1a989f94409b1e0a9acf94f940e7
Olwethu-Nene <230277845@mycput.ac.za>
2025-03-28 21:49:34 +0200

    Merge branch '230277845' of C:\Users\olwet\OneDrive\Documents\GitHub\Residence-Management-System with conflicts.

--- src/main/java/za/ac/cput/domain/Main.java
+++ src/main/java/za/ac/cput/domain/Main.java
@@ -1,23 +1,6 @@
-//222260971 Thapelo Ngwenya
-//230640893 Phindile Lisa Ngozi
-//230023967 Mengezi Junior Ngwenya
-//230666426 Tsireledzo Netshilonwe
-//230741754 Onako Ntsaluba
-// 230277845 Olwethu Nene
-
-
-
-package za.ac.cput.domain;
-
-
-
 
     public static void main(String[] args) {
-
-        }
-
-
-
+    }
 
 
 
---
+++ src/test/java/za/ac/cput/domain/factory/RoomTest.java
@@ -0,0 +1,31 @@
+package za.ac.cput.domain.factory;
+
+import org.junit.jupiter.api.Test;
+
+class RoomFactoryTest {
+
+    @Test
+    void getRepository() {
+        
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
